b1bdfcd0a4a5d50f6615399ac906dc5c62e840ab
Guillaume DG <guillaume.dusseuxgicquel@gmail.com>
2020-03-15 00:57:04 +0100

    css

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -55,6 +55,7 @@ export default function CharacterCard({ id, index }) {
 }
 
 const Card = styled.div`
+  height: 220px;
   box-shadow: 0 1px 3px rgba(0, 0, 0.12), 01px 2px rgba(0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   &:hover {
